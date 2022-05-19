d7190269c2658a7174380df7a606f175f24fe076
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-05-19 17:36:50 +0800

    Remove console log for navbar js

--- assets/js/navBar.js
+++ assets/js/navBar.js
@@ -1,5 +1,4 @@
 const navLink = document.querySelectorAll(".nav-link");
-console.log(navLink);
 for (const navLinks of navLink) {
   navLinks.addEventListener("click", function (event) {
     document.getElementById("menu-btn").click();

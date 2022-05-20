3562013329f4fed540f02817b27e2ad273ddb11e
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-05-20 19:03:36 +0800

    Update redirect url

--- assets/js/redirectToHome.js
+++ assets/js/redirectToHome.js
@@ -1,3 +1,3 @@
 setTimeout(function () {
-  window.location.href = "https://louisordonez.github.io/product-landing-page";
+  window.location.href = "https://louisordonez.github.io/product-landing-page/";
 }, 5000);

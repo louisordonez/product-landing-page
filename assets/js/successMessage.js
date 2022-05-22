7dda6a473a438470356d2d0fce8f94f5cc282a5d
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-05-22 14:26:11 +0800

    Improve dark mode toggle js

--- assets/js/successMessage.js
+++ assets/js/successMessage.js
@@ -1,9 +1,7 @@
 (() => {
   const searchParams = new URLSearchParams(window.location.search);
   const email = searchParams.get("email");
-  console.log(email);
   const domName = document.querySelector("#email");
-  console.log(domName);
 
   if (domName) {
     domName.textContent =
